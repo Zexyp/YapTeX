ce60ca2468928525f78473ed4ea1dd71f7b8bb11
Zexyp <zexyp01@gmail.com>
2025-12-01 21:26:35 +0100

    font pull mechanism rework for ttf

---
+++ scripts/run-tests.bat
@@ -0,0 +1,3 @@
+python -m unittest
+
+pylint src/yaptex/**/*.py
--- src/yaptex/fonts.py
+++ src/yaptex/fonts.py
@@ -3,6 +3,8 @@
 import os
 import re
 from .log import log_error, log_info, log_debug
+import json
+from textwrap import dedent
 
 try:
     import requests
@@ -12,28 +14,90 @@ except ImportError:
 
 from . import PATH_DIR_RESOURCE
 
-HOST = "https://fonts.googleapis.com/"
-GET_CSS = HOST + "css?family={family}:bold,italic,bolditalic,normal"
+HOST = "https://fonts.google.com/"
+GET_LIST = HOST + "download/list?family={family}"
 
 def download(family: str):
     """download font by family name"""
-    response = requests.get(GET_CSS.format(family=family), timeout=None)
-    assert response.status_code == 200
+    url = GET_LIST.format(family=family)
+    log_debug(f"requesting '{url}'")
+    # can't have shit when google
+    #headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0'}
+    response = requests.get(url, timeout=None)
+    assert response.status_code == 200, f"was {response.status_code}"
 
+    json_data = json.loads(response.text.removeprefix(")]}'"))
+    
     font_dir = os.path.join(PATH_DIR_RESOURCE, "font", family)
     os.makedirs(font_dir, exist_ok=True)
-    def replacor(m):
-        url = m.group(2)
-        bn = os.path.basename(url)
-        log_debug(f"downloadding '{url}'")
-        font_response = requests.get(url, timeout=None)
+    names = []
+    # iterate manifest files
+    for el_file in json_data["manifest"]["fileRefs"]:
+        filename = el_file["filename"]
+        if not filename.endswith(".ttf"): # quick drop
+            log_debug(f"skippping '{filename}'")
+            continue
+        
+        font_url = el_file["url"]
+        log_debug(f"downloading '{filename}' ({font_url})")
+        
+        font_response = requests.get(font_url)
+        assert font_response.status_code == 200, f"was {font_response.status_code}"
+
+        bn = os.path.basename(filename)
+        assert bn not in names
+        names.append(bn) # capture filename
         with open(os.path.join(font_dir, bn), mode="wb") as file:
             file.write(font_response.content)
 
-        return m.group(1) + bn + m.group(3)
+    log_info("building css")
 
-    css = re.sub(r"(src:\surl\()([^)]*)(\))", replacor, response.text)
+    
+    names.sort(key=lambda x: len(x))
+
+    src_regular = next(iter([n for n in names if "regular" in n.lower()]))
+    src_bolditalic = next(iter([n for n in names if "bolditalic" in n.lower()])) or src_regular
+    src_italic = next(iter([n for n in names if "italic" in n.lower()])) or src_regular
+    src_bold = next(iter([n for n in names if "bold" in n.lower()])) or src_regular
+
+    log_debug(f"regular:    '{src_regular}'")
+    log_debug(f"italic:     '{src_italic}'")
+    log_debug(f"bold:       '{src_bold}'")
+    log_debug(f"bolditalic: '{src_bolditalic}'")
+
+    css = f"""
+    /* YapTeX generated */
+    @font-face {{
+        font-family: '{family}';
+        font-weight: normal;
+        font-style: normal;
+        src: url({src_regular});
+    }}
+
+    @font-face {{
+        font-family: '{family}';
+        font-weight: normal;
+        font-style: italic;
+        src: url({src_italic});
+    }}
+
+    @font-face {{
+        font-family: '{family}';
+        font-weight: bold;
+        font-style: normal;
+        src: url({src_bold});
+    }}
+
+    @font-face {{
+        font-family: '{family}';
+        font-weight: bold;
+        font-style: italic;
+        src: url({src_bolditalic});
+    }}
+    """
+    css = dedent(css).lstrip()
+
     with open(os.path.join(font_dir, "style.css"), mode="w") as file:
         file.write(css)
-    
+
     log_info("font downloaded")
